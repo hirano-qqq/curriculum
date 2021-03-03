c58e98dee312a7cfaef7279f60eed1579ac8731f
hirano-qqq <daisangen998@gmail.com>
2021-03-03 10:31:14 +0900

    訂正2(制御文（条件分岐）)

--- JavaStudy/Task1_4.java
+++ JavaStudy/Task1_4.java
@@ -51,7 +51,7 @@ public class Task1_4 {
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-       }else if (pass.equals(CONST_MSG_ERROR_NAME)) {
+       }else if (pass.equals(USER_PASSWORD)) {
     System.out.println(CONST_MSG_ERROR_NAME); 
 
 
