d124756d2aa8e4ac3a8bbd3f901deb1976000339
hirano-qqq <daisangen998@gmail.com>
2021-04-04 19:36:54 +0900

    2章 チェックテスト

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -2,66 +2,48 @@ package check;
 
 import java.lang.reflect.Method;
 
-
 public class Check {
-	public static void main(String[] args)throws Exception{
+	public static void main(String[] args) throws Exception {
 
 		Class<Myname> c = Myname.class;
 
 		Myname m = new Myname();
 
-		Method m2 = c.getDeclaredMethod( "printName", String.class, String.class);
-		m2.setAccessible( true );
-		m2.invoke( m,m.getFirstName() ,m.getLastName() );
-
-
-
+		Method m2 = c.getDeclaredMethod("printName", String.class, String.class);
+		m2.setAccessible(true);
+		m2.invoke(m, m.getFirstName(), m.getLastName());
 
 		Pet p = new Pet();
 		RobotPet rp = new RobotPet();
 
-
-
 		constants.Constants co = new constants.Constants();
 
-
-
-		p=new Pet(co.CHECK_CLASS_JAVA,co.CHECK_CLASS_HOGE);
+		p = new Pet(co.CHECK_CLASS_JAVA, co.CHECK_CLASS_HOGE);
 		p.introduce();
 
-		rp=new RobotPet(co.CHECK_CLASS_R2D2,co.CHECK_CLASS_LUKE);
+		rp = new RobotPet(co.CHECK_CLASS_R2D2, co.CHECK_CLASS_LUKE);
 		rp.introduce();
 
-
-
-
-
-
+	}
 }
-}
-	class Myname{
 
-		private String firstName ="平野";
-		public String getFirstName() {
-			return firstName;
-		}
+class Myname {
 
-		private String lastName ="湧太";
-		public String getLastName() {
-			return lastName;
-		}
+	private String firstName = "平野";
 
-		private void printName(String x, String y) {
-			System.out.println("printNameメソッド→"+x+y);
+	public String getFirstName() {
+		return firstName;
+	}
 
+	private String lastName = "湧太";
 
+	public String getLastName() {
+		return lastName;
+	}
 
-
-
-}
+	private void printName(String x, String y) {
+		System.out.println("printNameメソッド→" + x + y);
 
 	}
 
-
-
-
+}
